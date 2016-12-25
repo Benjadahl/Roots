376098f243dae7a46ceb7c7216b6e8908377777b
Benjadahl <benjadahl@gmail.com>
2016-12-25 16:05:21 +0100

    Added function that will remove links from tabList when a tab is closed

--- app/background.js
+++ app/background.js
@@ -12,8 +12,8 @@ chrome.runtime.onMessage.addListener(
         }
         //Respond with the link array for the correct tab id
         if(typeof request.tabID !== "undefined"){
+            console.log("Sending links for tab " + request.tabID);
             if(typeof linksPerTab[request.tabID] !== "undefined"){
-                console.log("Sending links for tab " + request.tabID);
                 sendResponse({links: linksPerTab[request.tabID]});
             }
         }
@@ -22,6 +22,12 @@ chrome.runtime.onMessage.addListener(
 
 //Deal with the fact that chrome might replace a tab at any point in time
 chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
+    console.log("Replacing tab " + removedTabId + " with " + addedTabId);
     linksPerTab[addedTabId] = linksPerTab[removedTabId];
     delete linksPerTab[removedTabId];
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    delete linksPerTab[tabId];
+    console.log(linksPerTab);
 });
