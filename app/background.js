b35a160d8b09cedfb9d71b34778911fc02561f56
Benjadahl <benjadahl@gmail.com>
2016-12-25 01:06:22 +0100

    Fixed a bug where Chrome would replace a tabId and break the plugin, also added css

--- app/background.js
+++ app/background.js
@@ -4,12 +4,13 @@ var linksPerTab = {};
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
+        //Save the link array that is sent from content script
         if(typeof request.links !== "undefined"){
             console.log("Saving links for tab " + sender.tab.id);
             linksPerTab[sender.tab.id] = request.links;
-
             console.log(linksPerTab);
         }
+        //Respond with the link array for the correct tab id
         if(typeof request.tabID !== "undefined"){
             if(typeof linksPerTab[request.tabID] !== "undefined"){
                 console.log("Sending links for tab " + request.tabID);
@@ -18,3 +19,9 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+
+//Deal with the fact that chrome might replace a tab at any point in time
+chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
+    linksPerTab[addedTabId] = linksPerTab[removedTabId];
+    delete linksPerTab[removedTabId];
+});
